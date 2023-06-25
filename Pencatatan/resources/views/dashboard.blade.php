90fc5ca50f6e18ff9e4ef9563d5add09dbde6e46
candragunawan234 <dedenaum@gmail.com>
2023-06-25 19:34:38 +0700

    dashboard

---
+++ Pencatatan/resources/views/dashboard.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="css/styledb.css">
+</head>
+<body>
+  <div class="container">
+    <div id="navbar">
+      <div class="logo">Sistem Informasi Klinik Melati</div>
+      <div class="account" onclick="openAccountPage()"><i class="fas fa-user"></i> Akun</div>
+    </div>
+  
+    <div class="d-flex flex-grow-1">
+      <div id="sidebar">
+        <ul>
+          <li><a href="#"><i class="fas fa-home"></i> Beranda</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fas fa-database"></i> Data Master</a>
+            <ul class="dropdown-menu">
+              <li><a href="kelahiran.blade.php"><i class="fas fa-baby"></i> Data Kelahiran</a></li>
+              <li><a href="pengguna.blade.php"><i class="fas fa-users"></i> Data Pengguna</a></li>
+              <li><a href="jadwal.blade.php"><i class="far fa-calendar-alt"></i> Jadwal</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div class="logout">
+          <button class="btn btn-danger" href>Logout</button>
+        </div>
+      </div>
+      <div id="content">
+        <div class="center-container">
+          <div class="table-container">
+            <table class="table table-info">
+              <thead>
+                <tr>
+                  <th>Senin, 1 April 2023</th>
+                  <th>07.00 WIB</th>
+                  <th>Bidan Deliza Nesya</th>
+                </tr>
+              </thead>
+              {{-- <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>John Doe</td>
+                  <td>25</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jane Smith</td>
+                  <td>30</td>
+                </tr>
+              </tbody> --}}
+            </table>
+          </div>
+          <div class="img-container"></div>
+          <div class="img">
+            <img src="img/waduh.png" alt="Deskripsi Gambar" class="center-image" width="650" height="300">
+            <div class="text-overlay">
+                <h2>Selamat Datang </h2>
+                <h3> Bidan Deliza</h3>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    function openAccountPage() {
+      // Kode untuk membuka halaman akun
+      console.log('Halaman akun dibuka');
+    }
+  </script>
+</body>
+</html>
